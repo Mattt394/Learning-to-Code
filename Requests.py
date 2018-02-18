14e7b100f2a1c1020bdc6bd48fdc77ac6d18f01d
Mattt394 <36588863+Mattt394@users.noreply.github.com>
2018-02-19 10:15:03 +1300

    Update Requests.py

--- Requests.py
+++ Requests.py
@@ -7,16 +7,18 @@ from bs4 import BeautifulSoup
 r = requests.get('https://nzherald.co.nz')
 #getting html
 web_page = r.text
-
+# Passing to BeautifulSoup
 workpls = BeautifulSoup(web_page)
 
-
+#Matching all the titles, which were all h3 at this website
 titles = workpls.find_all('h3')
 
+#Saving to text file
 filee = open('C:/Users/Matt/Downloads/Life man/Crypto/datasci/Python programs/Titles.txt', 'w')
 
-
+#Writing to text file
 for i in range(0,len(titles)):
     filee.write(titles[i].get_text() +'\n')
-    
+
+filee.close()
 print('File created!')
